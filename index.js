5934feb4dea4911db18e64b6d8292076947b158d
Rishabh Bohra <rishabh.bohra@quicfind.com>
2019-06-08 03:35:32 +0530

    Repo cleanup and minor additions.

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ admin.initializeApp({
 var app = express();
 //Creating the route to get the registration token value from the client.
 app.get('/:token', function(req, resp){
-    var token = req.params.token.token
-    console.debug(token)
+    var token = req.params.token.token;
+    console.debug(token);
     
     var payload = {
         notification: {
@@ -28,7 +28,7 @@ app.get('/:token', function(req, resp){
       };
     
     //Waiting for some time to send the push notification to the client.
-    setTimeout (()=>{},4000)
+    setTimeout (()=>{},4000);
     //Sending the push notification to the client.
     admin.messaging().sendToDevice(registrationToken, payload, options)
       .then(function(response) {
@@ -41,4 +41,4 @@ app.get('/:token', function(req, resp){
 
 
 //Listening to the request at port 8000.
-app.listen(8800, () => console.log(`Example app listening on port 8800!`))
+app.listen(8800, () => console.log(`Example app listening on port 8800!`));
